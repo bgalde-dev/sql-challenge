01c6e788dcdb2b751d6a6dc51ad9f881d5c2a003
Ben Galde <bgalde.dev@gmail.com>
2021-02-01 18:10:21 -0800

    Initial query for assignment

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,83 @@
+-- Make sure all tables are populated
+select * from salaries;
+select * from employees;
+select * from departments;
+select * from dep_emp;
+select * from dep_manager;
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT emp_no AS "employee number", 
+    last_name AS "last name", 
+    first_name AS "first name", 
+    sex AS "sex", 
+    salaries.salary AS "salary" FROM employees
+INNER JOIN ON salaries
+WHERE employees.emp_no = salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name AS "first name", 
+	last_name AS "last name",     
+    hire_date AS "hire date"
+FROM employees
+WHERE date_part('year',hire_date) = 1986;
+
+-- List the manager of each department with the following information: department number, 
+-- department name, the manager's employee number, last name, first name.
+SELECT 
+	dep_manager.dept_no AS "Department Number",
+	departments.dept_name AS "Department Name",
+	employees.emp_no AS "employee number", 
+    employees.last_name AS "last name", 
+    employees.first_name AS "first name" 
+    FROM employees
+INNER JOIN dep_manager ON dep_manager.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dep_manager.dept_no;
+
+-- List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_no AS "employee number", 
+    employees.last_name AS "last name", 
+    employees.first_name AS "first name",
+	departments.dept_name AS "Department Name"
+FROM employees
+INNER JOIN dep_emp ON dep_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dep_emp.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT 
+    employees.first_name AS "first name",
+    employees.last_name AS "last name", 
+	employees.sex AS "sex"
+FROM employees
+WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_no AS "employee number", 
+    employees.last_name AS "last name", 
+    employees.first_name AS "first name",
+	departments.dept_name AS "Department Name"
+FROM employees
+INNER JOIN dep_emp ON dep_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dep_emp.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT 
+	employees.emp_no AS "employee number", 
+    employees.last_name AS "last name", 
+    employees.first_name AS "first name",
+	departments.dept_name AS "Department Name"
+FROM employees
+INNER JOIN dep_emp ON dep_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dep_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT 
+	employees.last_name AS "last name", 
+    COUNT(employees.last_name) AS "Number with Last Name"
+FROM employees
+GROUP BY employees.last_name
+ORDER BY COUNT(employees.last_name) DESC;
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sql-challenge
 UCSD SQL Challenge
+
+# Data assumptions
+There can be multiple managers for each department
+An employee can bee part of multiple departments
+Title names are unique
+Department names are unique
+
+# Load table order
+titles
+departments
+employees
+salaries
+dep_emp
+dep_manager
